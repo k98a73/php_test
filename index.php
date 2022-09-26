e7a8be248fb884dcc6778e6e83c89810f1df7c11
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-26 15:12:05 +0900

    PHPの定数

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-// 変数 動的型付
-$test_1 = 123;
-$test_2 = 456;
+// 定数
+const MAX = 10;
 
-$test_3 = $test_1 . $test_2;
-// 先頭は英文字かアンダーバー
-// $test = 'テストです';
-
-var_dump($test_3);
-// echo $test;
+echo MAX;
 ?>
