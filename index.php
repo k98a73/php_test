4b6689b472bbfd96432ce5d4cb3bec2c4db54d92
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-26 15:43:44 +0900

    PHPの配列

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
-// 定数
-const MAX = 10;
+// 配列1行
+$array_1 = ['あああ', 2, 3];
+// 配列複数行
+$array_2 = [
+  ['赤', '青', '黄'],
+  ['緑', '紫', '黒']
+];
 
-echo MAX;
+// echo $array[1];
+echo ('<pre>');
+var_dump($array_2);
+echo ('</pre>');
+
+echo $array_2[1][1];
 ?>
