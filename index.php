6bc5fb64bb0e95e2714ef6e3454f027ef1cd7ca9
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-26 15:08:03 +0900

    PHPの変数 その2

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 // 変数 動的型付
-$test = 123;
+$test_1 = 123;
+$test_2 = 456;
+
+$test_3 = $test_1 . $test_2;
+// 先頭は英文字かアンダーバー
 // $test = 'テストです';
 
-var_dump($test);
+var_dump($test_3);
 // echo $test;
 ?>
