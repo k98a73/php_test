a05bb889006209a10dac1ceb8f3720cf56bae576
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-27 10:04:36 +0900

    PHPの連想配列 その1

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-// 配列1行
-$array_1 = ['あああ', 2, 3];
-// 配列複数行
-$array_2 = [
-  ['赤', '青', '黄'],
-  ['緑', '紫', '黒']
+// 連想配列
+$array_member = [
+  'name' => '本田',
+  'height' => 170,
+  'hobby' => 'サッカー'
 ];
+echo $array_member['hobby'];
 
-// echo $array[1];
-echo ('<pre>');
-var_dump($array_2);
-echo ('</pre>');
-
-echo $array_2[1][1];
+echo '<pre>';
+var_dump($array_member);
+echo '</pre>';
 ?>
