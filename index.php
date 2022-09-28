83d24f9bc6b7b9fe2ef47d156982f424dec36d59
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-28 17:15:53 +0900

    PHPのif文 その3

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
 
-$height = 90;
+$height = 91;
 
-if ($height >= 91) {
-  echo '身長は' . $height . 'cmです';
+if ($height !== 90) {
+  echo '身長は90cmではありません。';
 }
-if ($height <= 90) {
-  echo '身長は' . $height . 'cmです';
+
+// データが入っているか
+// isset empty is_null
+
+$test = '1';
+
+if (empty($test)) {
+  echo '変数は空です。';
 } else {
-  echo '身長は' . $height . 'cmではありません';
+  echo '変数は空ではありません。';
 }
+
+// AND と OR
+
+$signal_1 = 'red';
+$signal_2 = 'yellow';
+
+if ($signal_1 === 'red' || $signal_2 === 'blue') {
+  echo '赤です。';
+}
+
+// 三項演算子
+
+$math = 80;
+
+$comment = $math > 80 ? 'good' : 'not good';
+
+echo $comment;
