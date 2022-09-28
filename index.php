d2258ec1c2f961dbe1d28fa486d75cd8f74f6120
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-28 16:40:28 +0900

    PHPのif文 その2

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-$height = '90';
 
-var_dump($height);
+$height = 90;
 
-if ($height === 90) {
-  echo '身長は' . $height . 'cmです。';
+if ($height >= 91) {
+  echo '身長は' . $height . 'cmです';
+}
+if ($height <= 90) {
+  echo '身長は' . $height . 'cmです';
+} else {
+  echo '身長は' . $height . 'cmではありません';
 }
