b31609c924647395478292462d5f1f7d0f9d8ac5
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-30 10:50:24 +0900

    関数を自作

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
 <?php
-// 文字列の長さ
 
-// マルチバイト
-// 日本語 SJIS(昔), UTF-8(主流) 3〜6バイト
-$text = 'あいうえお';
-// echo strlen($text);
+// snakeCase
+// check_postal_code();
 
-echo mb_strlen($text);
+$postalCode = '123-45678';
 
-echo '<br>';
+// camelCase
+function checkPostalCode($str) {
+  $replaced = str_replace('-', '', $str);
+  $length = strlen($replaced);
 
-// 文字列の置換
-
-$str = '文字列を置換します';
-
-echo str_replace('置換', 'ちかん', $str);
-
-echo '<br>';
-
-// 指定文字列で分割
-
-$str_2 = '指定文字列で、分割します';
-echo '<pre>';
-var_dump(explode('、', $str_2));
-echo '</pre>';
-
-// implode：文字列を結合
-
-echo '<br>';
-
-// 正規表現
-// 文字かどうか、数字かどうか、郵便番号やメールアドレスかどうか
-
-$str_3 = '特定の文字列が含まれるかを確認する';
-
-echo preg_match('/文字列/', $str_3);
-
-echo '<br>';
-
-// 指定文字列から文字列を取得する
-
-echo substr('abcde', 2);
-echo '<br>';
-
-echo mb_substr('かきくけこ', 2);
-echo '<br>';
-
-// 配列に配列を追加する
-$array = ['りんご', 'みかん'];
-
-array_push($array, 'ぶどう', 'なし');
-
-echo '<pre>';
-var_dump($array);
-echo '</pre>';
+  var_dump($length);
+  if ($length === 7) {
+    return true;
+  }
+  return false;
+}
+var_dump(checkPostalCode($postalCode));
