f719cb280e156d0dd61dd158348d1b4ad8ffdaa5
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-03 11:37:53 +0900

    戻るボタン

--- form/input.php
+++ form/input.php
@@ -7,10 +7,10 @@ if (!empty($_POST)) {
 
 // 入力、確認、完了のページを1つのファイルで切り分けるための変数
 $pageFlag = 0;
+
 if (!empty($_POST['btn_confirm'])) {
   $pageFlag = 1;
 }
-$pageFlag = 1;
 if (!empty($_POST['btn_submit'])) {
   $pageFlag = 2;
 }
@@ -26,10 +26,18 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageFlag === 0): ?>
     <form method="POST" action="input.php">
       氏名
-      <input type="text" name="your_name">
+      <input type="text" name="your_name" value="<?php if (
+        !empty($_POST['your_name'])
+      ) {
+        echo $_POST['your_name'];
+      } ?>">
       <br>
       メールアドレス
-      <input type="email" name="email">
+      <input type="email" name="email" value="<?php if (
+        !empty($_POST['email'])
+      ) {
+        echo $_POST['email'];
+      } ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
     </form>
@@ -43,6 +51,7 @@ if (!empty($_POST['btn_submit'])) {
       メールアドレス
       <?php echo $_POST['email']; ?>
       <br>
+      <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信する">
       <input type="hidden" name="your_name" value="<?php echo $_POST[
         'your_name'
