667600ca13ee459e649c5016bbaed91d14984601
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-04 16:10:21 +0900

    バリデーションの準備1

--- form/input.php
+++ form/input.php
@@ -4,9 +4,9 @@ session_start();
 
 header('X-FRAME-OPTIONS:DENY');
 
-if (!empty($_SESSION)) {
+if (!empty($_POST)) {
   echo '<pre>';
-  var_dump($_SESSION);
+  var_dump($_POST);
   echo '</pre>';
 }
 
@@ -54,6 +54,35 @@ if (!empty($_POST['btn_submit'])) {
       ) {
         echo h($_POST['email']);
       } ?>">
+      <br>
+      ホームページ
+      <input type="url" name="url" value="<?php if (!empty($_POST['url'])) {
+        echo h($_POST['url']);
+      } ?>">
+      <br>
+      性別
+      <input type="radio" name="gender" value="0">男性
+      <input type="radio" name="gender" value="1">女性
+      <br>
+      年齢
+      <select name="age">
+        <option value="">選択してください</option>
+        <option value="1">〜19歳</option>
+        <option value="2">20歳〜29歳</option>
+        <option value="3">30歳〜39歳</option>
+        <option value="4">40歳〜49歳</option>
+        <option value="5">50歳〜59歳</option>
+        <option value="6">60歳〜</option>
+      </select>
+      <br>
+      お問い合わせ内容
+      <textarea name="contact">
+        <?php if (!empty($_POST['contact'])) {
+          echo h($_POST['contact']);
+        } ?>
+      </textarea>
+      <br>
+      <input type="checkbox" name="caution" value="1">注意事項にチェックする
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
       <input type="hidden" name="csrf" value="<?php echo $token; ?>">
