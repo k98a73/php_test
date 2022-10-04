b4393c33b8c02054a320e26bcf9651df0d876b1b
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-04 14:43:14 +0900

    フォームセキュリティ XSS

--- form/input.php
+++ form/input.php
@@ -1,8 +1,15 @@
 <?php
-if (!empty($_POST)) {
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
+
+header('X-FRAME-OPTIONS:DENY');
+
+//   if (!empty($_POST)) {
+//   echo '<pre>';
+//   var_dump($_POST);
+//   echo '</pre>';
+// }
+
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 // 入力、確認、完了のページを1つのファイルで切り分けるための変数
@@ -29,14 +36,14 @@ if (!empty($_POST['btn_submit'])) {
       <input type="text" name="your_name" value="<?php if (
         !empty($_POST['your_name'])
       ) {
-        echo $_POST['your_name'];
+        echo h($_POST['your_name']);
       } ?>">
       <br>
       メールアドレス
       <input type="email" name="email" value="<?php if (
         !empty($_POST['email'])
       ) {
-        echo $_POST['email'];
+        echo h($_POST['email']);
       } ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
@@ -46,17 +53,19 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageFlag === 1): ?>
     <form method="POST" action="input.php">
       氏名
-      <?php echo $_POST['your_name']; ?>
+      <?php echo h($_POST['your_name']); ?>
       <br>
       メールアドレス
-      <?php echo $_POST['email']; ?>
+      <?php echo h($_POST['email']); ?>
       <br>
       <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信する">
-      <input type="hidden" name="your_name" value="<?php echo $_POST[
-        'your_name'
-      ]; ?>">
-      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+      <input type="hidden" name="your_name" value="<?php echo h(
+        $_POST['your_name'],
+      ); ?>">
+      <input type="hidden" name="email" value="<?php echo h(
+        $_POST['email'],
+      ); ?>">
     </form>
   <?php endif; ?>
 
