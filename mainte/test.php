cd174931bc72fda7cbbb5fa42afef3e30fb6251b
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-07 16:36:57 +0900

    ファイル操作 その2

--- mainte/test.php
+++ mainte/test.php
@@ -2,21 +2,10 @@
 
 $contactFile = '.contact.dat';
 
-$fileContents = file_get_contents($contactFile);
+$contents = fopen($contactFile, 'a+');
 
-// echo $fileContents;
+$addText = '1行追加' . "\n";
 
-// file_put_contents($contactFile, 'テストです');
+fwrite($contents, $addText);
 
-// $addText = 'テストです' . "\n";
-
-// file_put_contents($contactFile, $addText, FILE_APPEND);
-
-$allDdata = file($contactFile);
-
-foreach ($allDdata as $lineData) {
-  $lines = explode(',', $lineData);
-  echo $lines[0] . '<br>';
-  echo $lines[1] . '<br>';
-  echo $lines[2] . '<br>';
-}
+fclose($contents);
