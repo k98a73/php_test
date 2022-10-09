9ad20ffc5edab5e87e64793841fb15569953afd4
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-09 16:12:03 +0900

    PDO プリペアードステートメント プレースホルダ

---
+++ mainte/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'db_connection.php';
+
+// ユーザー入力なし query
+// sql
+// $sql = 'select * from contacts where id = 2';
+// sql実行 stmt：ステートメントの略
+// $stmt = $pdo->query($sql);
+
+// $result = $stmt->fetchAll();
+
+// echo '<pre>';
+// var_dump($result);
+// echo '</pre>';
+
+//ユーザー入力あり prepare, bind, execute 悪意ユーザー delete * SQLインジェクション対策
+$sql = 'select * from contacts where id = :id';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', 3, PDO::PARAM_INT);
+$stmt->execute();
+
+$result = $stmt->fetchAll();
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
